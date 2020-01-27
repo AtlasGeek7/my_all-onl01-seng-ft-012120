37fc55a567e179a33a0e6f952a1e54276bbd289f
Khalid T <khalid.tissire@gmail.com>
2020-01-27 03:21:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   valArr = []
   while i < collection.length
-    valArr.push(collection[i])
+    valArr.push(yield(collection[i]))
     i += 1
   end
   return valArr.include?(false)
